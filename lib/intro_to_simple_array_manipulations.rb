746f551acba27242040191db586c8870f32e6ffc
John Mark Redding <johnmarkredding@gmail.com>
2018-11-08 20:06:07 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,8 +14,8 @@ end
 def using_shift(array)
   array.shift
 end
-def pop_with_args(array)
-  array.pop(2)
+def shift_with_args(array)
+  array.shift(2)
 end
 def pop_with_args(array)
   array.pop(2)
